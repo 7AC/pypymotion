67ce4b420432a1df7c8b08ef3f04125ef0966cd9
tAc <giusva@gmail.com>
2013-03-11 22:18:28 -0700

    Partial iOS compatibility

--- pypymotion.py
+++ pypymotion.py
@@ -28,6 +28,11 @@ try:
    playIcon = config.get( 'Email', 'play_icon' )
 except ConfigParser.NoOptionError:
    pass
+iosCompatible = False
+try:
+   iosCompatible = config.getint( 'Email', 'ios_compatible' )
+except ConfigParser.NoOptionError:
+   pass
 presenceMacs = []
 network = None
 try:
@@ -77,6 +82,13 @@ def pictures( dirpath ):
    # return the last N pictures before the postCapture ones
    return [ path for _, path in sorted( entries )[ - postCapture - 5 : - postCapture ] ]
 
+def convertForIos( path, video ):
+   baseName, _ = os.path.splitext( video )
+   iosVideo = path + '/' + baseName + '.mov'
+   subprocess.Popen( [ 'ffmpeg', '-i', video, iosVideo ], stdout=subprocess.PIPE,
+	 	     stderr=subprocess.STDOUT ).stdout.readlines()
+   return iosVideo
+
 def sendEmail( attachment ):
    #print '[%s] %s\n' % ( datetime.now(), attachment )
    msg = MIMEMultipart( 'alternative' )
@@ -84,18 +96,36 @@ def sendEmail( attachment ):
    msg[ 'From' ] = emailFrom
    msg[ 'To' ] = emailTo
 
+   if iosCompatible:
+      attachment = convertForIos( *os.path.split( attachment ) )
    duration = videoDuration( attachment )
    pics = pictures( picturesDir )
    embeddedPics = ''
    for p in pics:
-      embeddedPics += '<img src="cid:%s" width="640">' % os.path.basename( p )
-   html = '<html><head><title>motion</title></head><body>'
+      embeddedPics += '<div><img height="480" width="640" apple-width="yes" apple-height="yes" src="cid:%s"></div>' \
+	    	      % os.path.basename( p )
+
+'''
+<html>
+<body style="word-wrap: break-word; -webkit-nbsp-mode: space; -webkit-line-break: after-white-space; ">
+<span contenteditable="true" apple-content-name="body" style="display: block; ">
+Chris Writes Mail Template
+</span>
+<img src="http://www.chriswrites.com/wp-content/uploads/Article-12-Mountain-Lion-Icon.jpg">
+</body>
+</html>
+'''
+
+   html = '<html><head><meta http-equiv="Content-Type" content="text/html charset=us-ascii"></head>'
+   html += '<body style="word-wrap: break-word; -webkit-nbsp-mode: space; -webkit-line-break: after-white-space; ">'
    if videoUrl and playIcon:
-      html += '<a href="%s/%s"><img src="cid:%s" height="70" align="left"></a>' \
+      html += '<a href="%s/%s"><img src="cid:%s" height="48" width="48" apple-width="yes" apple-height="yes"></a>' \
 	      % ( videoUrl, os.path.basename( attachment ), os.path.basename( playIcon ) )
-   html += '<p>&nbsp;&nbsp;&nbsp;Duration: %ss</p>' % duration
-   html += '<p>&nbsp;&nbsp;&nbsp;Available space: %s</p>' % df()
-   html += '<br>%s</body></html>' % embeddedPics
+   html += '<div>Duration: %ss</div>' % duration
+   html += '<div>Available space: %s</div>' % df()
+   html += '<div><br></div><div>'
+
+   html += embeddedPics + '</body></html>'
 
    body = MIMEText( html, 'html' )
 
